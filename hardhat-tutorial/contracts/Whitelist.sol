0c13334ebe96d6fccd6392c49efdee5d0538b44c
AzureKn1ght <philsonnah@msn.com>
2022-08-13 23:27:21 +0800

    Deployed Whitelist contract

---
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
+
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted (default value of uint8 is zero).
+    // NOTE: Don't change this variable name, as it will be part of verification
+    uint8 public numAddressesWhitelisted;
+
+    // Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses can't be added, limit reached"
+        );
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
+}
+
+// Deployed to: https://goerli.etherscan.io/address/0xf51ccb5d3b54a5b4d08121945a6d64d9524894b9 
